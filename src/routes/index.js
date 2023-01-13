c4075171bbb262d7ea02e14dca7aee046f701194
yeffreycode <yeffreyespinoza200@gmail.com>
2023-01-13 09:08:57 -0500

    [FT/004] & new  route GET /api/projects/:id

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import { Router } from 'express'
 
 // Import Controllers
 import { register, login } from '../controllers/authController.js'
-import { createProyect, getAllProjects } from '../controllers/proyectController.js'
+import { createProyect, getAllProjects, getProjectById} from '../controllers/proyectController.js'
 
 import { protect } from '../middleware/protect.js'
 const router = Router()
@@ -20,5 +20,6 @@ router.post('/login', login)
 // Projet Routes
 router.post('/create-project', protect, createProyect)
 router.get("/projects", protect, getAllProjects);
+router.get('/projects/:id', protect, getProjectById)
 
 export default router
