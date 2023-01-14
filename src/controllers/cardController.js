785e6c551188b706b44e4fed278016704adcae58
yeffreycode <yeffreyespinoza200@gmail.com>
2023-01-14 13:05:15 -0500

    [FT/006] src/controllers/cardController.js & change logic request

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -9,19 +9,14 @@ import { ErrorCreator, ResponseCreator } from "../../utils/responseCreator.js";
 
 //  -----PRIVATE CONTROLLERS-----  //
 export const createCard = async (req, res, next) => {
-  const { question, answer, boxId } = req.body;
+  const { question, answer, projectId } = req.body;
   try {
-    const project = await Project.findOne({ "boxes._id": boxId });
+    const project = await Project.findById(projectId);
     if (!project) {
-      return next(new ErrorCreator("Box Not Found", 404));
+      return next(new ErrorCreator("project Not Found", 404));
     }
     const newCard = await Card.create({ question, answer });
-     project.boxes.map((item) => {
-      if (item._id == boxId) {
-        item.cards.push(newCard.id)
-      }
-      return item;
-    });
+    project.boxes[0].cards.push(newCard.id);
     await project.save();
     return res.send(new ResponseCreator("success", 200, { project, card: newCard}));
   } catch (err) {
