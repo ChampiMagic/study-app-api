1430f10feea89d84accad80c913e3d178edae217
yeffreycode <yeffreyespinoza200@gmail.com>
2023-01-16 14:10:45 -0500

    [FT/005] src/models/Tag.js & new Model Tag"

---
+++ src/models/Tag.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import mongooseUniqueValidator from 'mongoose-unique-validator'
+const { Schema, model } = mongoose
+
+const schema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+}, {
+  versionKey: false
+})
+
+schema.plugin(mongooseUniqueValidator)
+
+export default model('Box', schema)
