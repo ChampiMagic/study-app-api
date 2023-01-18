f405a0caa0569168beb384da30ee94a563c57e67
yeffreycode <yeffreyespinoza200@gmail.com>
2023-01-18 17:35:36 -0500

    [FT/013] src/controllers/tagController.js & update tag controller

--- src/controllers/tagController.js
+++ src/controllers/tagController.js
@@ -1,15 +1,23 @@
 // import model
 import User from "../models/user.js";
-
+import Tag from '../models/Tag.js';
 // import constructors
 import { ErrorCreator, ResponseCreator } from "../../utils/responseCreator.js";
 
 export const crateTag = async (req, res, next) => {
-  const newTag = { name: req.body.name };
+  const tag = {name: req.body.name}
   try {
-    const user = await User.findByIdAndUpdate(req.userData.id, { $push: { tags: newTag } }, { new: true });
+    const user = await User.findById(req.userData.id);
     if (!user) return next(new ErrorCreator("User Not Found", 404));
-    res.send(new ResponseCreator("Successfully created new Tag", 200, { user }));
+
+    //create tag
+    const newTag = await Tag.create(tag);
+
+    //udpate user
+    user.tags.push(newTag);
+    await user.save();
+
+    res.send(new ResponseCreator("Successfully created new Tag", 200, { user, tag: newTag }));
   } catch (err) {
     console.log("ERROR: PROYECTCONTROLLER(createTag)");
     next(err);
